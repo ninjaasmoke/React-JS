0285c45c870068a40d5f95c85b6b31d7d5f2ca09
Nithin Sai K J <nithins674@gmail.com>
2020-09-23 20:58:26 +0530

    Updates

---
+++ precache-manifest.e18b4cd54ffd276469c861f0bef846d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be1d56e0d06a59ff6d5facf8aee6152",
+    "url": "/React-JS/confusion/index.html"
+  },
+  {
+    "revision": "386cb80f0bde0a93358c",
+    "url": "/React-JS/confusion/static/css/2.7cef5511.chunk.css"
+  },
+  {
+    "revision": "c14cf025ebd6942dad2e",
+    "url": "/React-JS/confusion/static/css/main.a8546c2e.chunk.css"
+  },
+  {
+    "revision": "386cb80f0bde0a93358c",
+    "url": "/React-JS/confusion/static/js/2.6b09eb79.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-JS/confusion/static/js/2.6b09eb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14cf025ebd6942dad2e",
+    "url": "/React-JS/confusion/static/js/main.3f30b6f4.chunk.js"
+  },
+  {
+    "revision": "99a89280e3ddba0f767e",
+    "url": "/React-JS/confusion/static/js/runtime-main.81ac7a4e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-JS/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-JS/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-JS/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-JS/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-JS/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
